e54ef4ab6b7140c8cfce310997a07a83bfd4560c
WhiteInfinite <118209757+WhiteInfinite@users.noreply.github.com>
2024-11-17 06:15:36 +0530

    Update SUB16.asm

--- MPMC_LAB/emu 8086/SUB16.asm
+++ MPMC_LAB/emu 8086/SUB16.asm
@@ -8,7 +8,7 @@
 ; PROGRAM NAME		: SUBTRACTION OF 16 BIT NUMBERS
 ;=============================================================
 ; DESCRIPTION		: USING MEMORY MODEL
-; AUTHOR      		: ANANTHAKRISHNAN K
+; AUTHOR      		: LIYAN C K M
 ; SEGMENT DEFINITION	: [IMPLICIT]   
 ;=============================================================	(1)
 
