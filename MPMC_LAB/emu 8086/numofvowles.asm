0e945acc62bb6e2a51baebc066e57ec17b2e0b17
WhiteInfinite <118209757+WhiteInfinite@users.noreply.github.com>
2024-11-17 06:16:57 +0530

    Update numofvowles.asm

--- MPMC_LAB/emu 8086/numofvowles.asm
+++ MPMC_LAB/emu 8086/numofvowles.asm
@@ -8,7 +8,7 @@
 ; PROGRAM NAME		: COUNT THE NUMBER OF VOWELS
 ;=============================================================
 ; DESCRIPTION		: USING MEMORY MODEL
-; AUTHOR      		: ANANTHAKRISHNAN K
+; AUTHOR      		: LIYAN C K M
 ; SEGMENT DEFINITION	: [IMPLICIT]   
 ;=============================================================	(1)
 
