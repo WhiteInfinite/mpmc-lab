bf91da073defd589daec83db6eea5d6972312a67
WhiteInfinite <118209757+WhiteInfinite@users.noreply.github.com>
2024-11-17 06:14:19 +0530

    Update ADD32.asm

--- MPMC_LAB/emu 8086/ADD32.asm
+++ MPMC_LAB/emu 8086/ADD32.asm
@@ -8,7 +8,7 @@
 ; PROGRAM NAME		: ADDITION OF TWO 32 BIT NUMBERS
 ;=============================================================
 ; DESCRIPTION		: USING MEMORY MODEL
-; AUTHOR      		: ANANTHAKRISHNAN K
+; AUTHOR      		: LIYAN C K M
 ; SEGMENT DEFINITION	: [IMPLICIT]   
 ;=============================================================	(1)
 
