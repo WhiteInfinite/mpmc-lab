b45480c60fdd70cea383bd0dd8fd4594334a43d6
WhiteInfinite <118209757+WhiteInfinite@users.noreply.github.com>
2024-11-17 06:13:33 +0530

    Update uptolow.asm

--- MPMC_LAB/emu 8086/uptolow.asm
+++ MPMC_LAB/emu 8086/uptolow.asm
@@ -8,7 +8,7 @@
 ; PROGRAM NAME		: UPPER TO LOWER
 ;=============================================================
 ; DESCRIPTION		: USING MEMORY MODEL
-; AUTHOR      		: ANANTHAKRISHNAN K
+; AUTHOR      		: LIYAN C K M
 ; SEGMENT DEFINITION	: [IMPLICIT]   
 ;=============================================================	(1)
 
@@ -17,94 +17,47 @@
 
 ;=============================================================	(2)
 
-.DATA               	; Start of the data segment
+.DATA
+inputString db 'HELLO WORLD$'  
+outputString db 'Converted String: $'
 
-INPUT DB 'HELLO WORLD$','$'
-VOWEL DB 'AEIOUaeiou$','$'
-VOWEL_COUNT DW 0
-     
+
 ;=============================================================	(3)
 
 
 .CODE
-
 START:
+    mov ax, @data
+    mov ds, ax
 
-    MOV AX, @DATA       ; Initialize the data segment
+    mov ah, 09h                 
+    lea dx, outputString        
+    int 21h                     
 
-    MOV DS, AX
+    lea si, inputString        
 
-    MOV ES, AX
+convert_loop:
+    mov al, [si]               
+    cmp al, '$'                
+    je done                    
 
-    
+    cmp al, 'A'                
+    jb not_uppercase           
+    cmp al, 'Z'                
+    ja not_uppercase           
 
-    LEA SI, INPUT       ; Load the address of the INPUT string into SI
+    add al, 32                 
 
-    MOV CX, 0           ; Initialize the vowel counter to 0
+not_uppercase:
+    mov [si], al               
+    inc si                     
+    jmp convert_loop           
 
+done:
+    mov ah, 09h                
+    lea dx, inputString        
+    int 21h                    
 
-NEXT_CHAR:
-
-    LODSB               ; Load the next character from INPUT (DS:SI) into AL
-
-    CMP AL, '$'         ; Check if end of string
-
-    JE END_PROGRAM      ; If end of string, exit
-
-
-    PUSH AX             ; Save the current character (AL) on the stack
-
-    LEA DI, VOWEL      ; Load the address of the VOWELS string into DI
-
-
-CHECK_VOWEL:
-
-    LODSB               ; Load the next vowel from VOWELS (DS:DI) into AL
-
-    CMP AL, '$'         ; Check if end of the vowels list
-
-    JE NOT_VOWEL        ; If no match, exit vowel check
-
-
-    POP BX              ; Restore the saved character into BX
-
-    CMP BL, AL          ; Compare the character from INPUT with the current vowel
-
-    JE IS_VOWEL         ; If they are equal, it's a vowel
-
-
-    PUSH BX             ; Save the input character again for the next vowel check
-
-    JMP CHECK_VOWEL     ; Continue checking the next vowel
-
-
-IS_VOWEL:
-
-    INC CX              ; Increment vowel count
-
-    JMP NEXT_ITERATION  ; Go to the next character in INPUT
-
-
-NOT_VOWEL:
-
-    POP BX              ; Remove the input character from the stack
-
-
-NEXT_ITERATION:
-
-    JMP NEXT_CHAR       ; Continue with the next character in INPUT
-
-
-END_PROGRAM:
-
-    MOV VOWEL_COUNT, CX ; Store the final vowel count
-
-
-    MOV AH, 4CH         ; Terminate the program
-
-    INT 21H
-
-
-END START
-;=============================================================	(4)
-
+    mov ah, 4Ch                
+    int 21h                    
+ END START
